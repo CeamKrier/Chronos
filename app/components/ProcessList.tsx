7b85e015cc86bf77ff965617cc713c7392c42294
Emre Çakır <ceamkrier@gmail.com>
2020-10-03 23:36:11 +0300

    refactor: process list card layout improved

--- app/components/ProcessList.tsx
+++ app/components/ProcessList.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { RiZzzLine } from 'react-icons/ri';
+import { VscVmRunning } from 'react-icons/vsc';
 import { allProcesses } from '../features/observer/observerSlice';
 import CSS from './ProcessList.css';
 
@@ -12,7 +14,7 @@ export default function ProcessList() {
           <div className={CSS.processCard} key={process.windowPid}>
             <div className={CSS.processCardBody}>
               <div className={CSS.processCardBodyColumn}>
-                <span>{`Name: ${process.windowClass}`}</span>
+                <span>{`${process.windowClass}`}</span>
                 {/* <span>
                   PID:
                   {process.windowPid}
@@ -20,14 +22,14 @@ export default function ProcessList() {
               </div>
 
               <div className={CSS.processCardBodyColumn}>
-                <span>
-                  Usage:
+                <span className={CSS.processSpanElement}>
+                  <VscVmRunning size="1.4em" />
                   {` ${new Date((process.usageTime || 0) * 1000)
                     .toISOString()
                     .substr(11, 8)}`}
                 </span>
-                <span>
-                  Idle:
+                <span className={CSS.processSpanElement}>
+                  <RiZzzLine size="1.4em" />
                   {` ${new Date((process.idleTime || 0) * 1000)
                     .toISOString()
                     .substr(11, 8)}`}
