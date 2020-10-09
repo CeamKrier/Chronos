df656829a4b50c493c65a753ce175f0ea0ef35af
Emre Çakır <ceamkrier@gmail.com>
2020-10-09 17:35:24 +0300

    refactor: static color palette changed with a module that generates a color hex from arbitrary string

--- app/components/ProcessList.tsx
+++ app/components/ProcessList.tsx
@@ -2,41 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RiZzzLine } from 'react-icons/ri';
 import { VscVmRunning } from 'react-icons/vsc';
+import StringToColor from 'string-to-color';
 import {
   allProcesses,
   totalScreenTime,
 } from '../features/observer/observerSlice';
 import CSS from './ProcessList.css';
-
-// const normalizeProcessName = (rawName: string) => {
-//   // Process might be Chrome, VS Code as xxxx - xxxx - Google Chrome
-//   if (rawName.includes(' - ')) {
-//     const splittedName = rawName.split(' - ');
-//     if (splittedName[0] === 'Developer Tools') {
-//       return splittedName[0];
-//     }
-//     return splittedName[splittedName.length - 1];
-//   }
-//   return rawName;
-// };
-
-const colorPalette = [
-  '#005CE6',
-  '#4791FF',
-  '#40A077',
-  '#50B988',
-  '#91D4BC',
-  '#F3FFC6',
-  '#C3EB78',
-  '#B6174B',
-  '#CD7E7C',
-  '#E0AFA0',
-  '#FFAC81',
-  '#FF928B',
-  '#FEC3A6',
-  '#EFE9AE',
-  '#CDEAC0',
-];
 
 export default function ProcessList() {
   const processLog = useSelector(allProcesses);
@@ -44,13 +15,13 @@ export default function ProcessList() {
 
   return (
     <div className={CSS.processListWrapper}>
-      {processLog.map((process, index) => {
+      {processLog.map((process) => {
         return (
           <div className={CSS.processCard} key={process.id}>
             <div className={CSS.processCardBody}>
               <div
                 className={CSS.processColorIndicator}
-                style={{ background: colorPalette[index] }}
+                style={{ background: StringToColor(process.owner.name) }}
               />
               <div className={CSS.processCardBodyColumn}>
                 <span className={CSS.processName}>{process.owner.name}</span>
